d2556ab3eb9f1c15223537546bc9e515018144e4
Pratik Ghodekar <pratikg@citiustech.com>
2017-02-23 11:49:04 +0530

    first commit

---
+++ WebContent/index.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Calculator</title>
+</head>
+<body>
+
+	<h1>Calculator Application</h1>
+
+	<form method="get" action="/CalculatorApplication/calServlet">
+		<table>
+			<tr>
+				<td>Number 1:</td>
+				<td><input type="text" name="num1" />
+			</tr>
+
+
+			<tr>
+				<td>Number 2:</td>
+				<td><input type="text" name="num2" />
+			</tr>
+
+			<tr>
+				<td>Operation:</td>
+				<td><select name="operator">
+						<option value="add">+</option>
+						<option value="sub">&minus;</option>
+						<option value="mul">&lowast;</option>
+						<option value="div">&divide;</option>
+				</select></td>
+			</tr>
+
+			<tr>
+				<td><input type="submit" value="Calculate"></input> </td>
+
+				<td><input type="reset" value="Cancel"></input></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ src/main/java/com/calc/servlet/CalculatorServlet.java
@@ -0,0 +1,61 @@
+package com.calc.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.calc.service.Calculator;
+
+/**
+ * Servlet implementation class CalculatorServlet
+ */
+@WebServlet("/CalculatorServlet")
+public class CalculatorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	Calculator calObj;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CalculatorServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+		calObj = new Calculator();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+
+		String n1 = request.getParameter("num1");
+		System.out.println(n1);
+		String n2 = request.getParameter("num2");
+		System.out.println(n2);
+		String opt = request.getParameter("operator");
+		out.write("<html> <body> <h2> Result is: ");
+		out.write(" " + calObj.performAction(opt, n1, n2));
+		out.write("</h2> </body></html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
